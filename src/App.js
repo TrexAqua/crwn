e9f4036bd4b40f2d46f0d829141fa489bfb81fef
TrexAqua <mayankg92156@gmail.com>
2020-12-10 17:21:24 +0530

    added header with proper routes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React from "react";
+import "./App.css";
 import { Route, Switch } from "react-router-dom";
 import ShopPage from "./Screens/ShopPage";
 import Homepage from "./Screens/Homepage";
+import Header from "./Components/Header";
 
 const App = () => {
   return (
     <div>
+      <Header />
       <Switch>
         <Route exact path="/" component={Homepage} />
         <Route exact path="/shop" component={ShopPage} />
